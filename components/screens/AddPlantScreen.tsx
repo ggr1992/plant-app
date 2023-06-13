488d36381b3986933316c908e6fff5df75f96bb3
mik-rzo <michael.panong3@gmail.com>
2023-06-13 09:32:58 +0100

    added placeholder screens

---
+++ components/screens/AddPlantScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export function AddPlantScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Add Plant screen</Text>
+		</View>
+	)
+}
