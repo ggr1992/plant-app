e751e2375a17185665db88d278205238eafa6d7c
Kam Singh <kamisera@gmail.com>
2023-06-14 12:39:09 +0200

    refactors camera plant identification

--- components/screens/IdentifyPlantScreen.tsx
+++ components/screens/IdentifyPlantScreen.tsx
@@ -1,106 +1,151 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Button,
-  Image,
-} from "react-native";
-import { useEffect, useRef, useState } from "react";
-import { Camera } from "expo-camera";
+import React, { useState, useEffect, useRef, Fragment } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import { Camera, CameraType } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
+import { Entypo } from "@expo/vector-icons";
 import { identifyPlant } from "../utils/utils";
 
 export function IdentifyPlantScreen() {
-  let cameraRef = useRef();
-  const [hasCameraPermission, setHasCameraPermission] = useState();
-  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
-  const [photo, setPhoto] = useState();
-  const [res, setRes] = useState("");
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState([]);
+  const [bestMatch, setBestMatch] = useState("");
 
   useEffect(() => {
-    (async () => {
-      const cameraPermission = await Camera.requestCameraPermissionsAsync();
-      const mediaLibraryPermission =
-        await MediaLibrary.requestPermissionsAsync();
-      setHasCameraPermission(cameraPermission.status === "granted");
-      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
-    })();
+    async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === "granted");
+    };
   }, []);
 
   useEffect(() => {
-    if (photo && photo.hasOwnProperty("base64")) {
-      console.log(photo.base64);
-      identifyPlant(photo.base64).then((data) => {
-        setRes(data);
+    if (isLoading && image) {
+      identifyPlant(image).then((data) => {
+        setBestMatch(data.bestMatch);
+        setResults(data.results.slice(0, 5));
+        setIsLoading(false);
       });
     }
-  }, [photo]);
+  });
 
-  if (hasCameraPermission === undefined) {
-    return <Text>Requesting permissions...</Text>;
-  } else if (!hasCameraPermission) {
-    return (
-      <Text>
-        Permission for camera not granted. Please change this in settings.
-      </Text>
-    );
-  }
-
-  let takePic = async () => {
-    let options = {
-      quality: 1,
-      base64: true,
-      exif: false,
-    };
-
-    let newPhoto = await cameraRef.current.takePictureAsync(options);
-    setPhoto(newPhoto);
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        setImage(data.uri);
+        setIsLoading(true);
+      } catch (err) {
+        console.log(err);
+      }
+    }
   };
 
+  const resetPicture = () => {
+    setImage(null);
+    setIsLoading(false);
+  };
+
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera!</Text>;
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      {photo && photo.hasOwnProperty("base64") && (
-        <Text>Photo: {photo?.base64}</Text>
-      )}
-      {/* {res !== undefined && <Text>response is: {JSON.stringify(res)}</Text>} */}
-      {!photo && (
-        <Camera style={styles.container} ref={cameraRef}>
-          <View style={styles.buttonContainer}>
-            <Button title="Take Pic" onPress={takePic} />
-          </View>
-          <StatusBar style="auto" />
+    <View style={styles.container}>
+      {!image ? (
+        <Camera
+          style={styles.camera}
+          type={type}
+          flashMode={flash}
+          ref={cameraRef}
+        >
+          <TouchableOpacity
+            onPress={takePicture}
+            style={{
+              height: 40,
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Entypo name="camera" size={28} color={"#fff"} />
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 16,
+                color: "#f1f1f1",
+                marginLeft: 10,
+              }}
+            >
+              Take Pic
+            </Text>
+          </TouchableOpacity>
         </Camera>
-      )}
-
-      {photo !== undefined && (
+      ) : (
         <>
-          <Image
-            style={styles.preview}
-            source={{ uri: "data:image/jpg;base64," + photo.base64 }}
-          />
-
-          <Button title="Try again" onPress={() => setPhoto(undefined)} />
+          {isLoading && (
+            <Text style={{ color: "#333", marginBottom: 40 }}>
+              Trying to identify plant... Please wait...
+            </Text>
+          )}
+          {!isLoading && results.length === 0 && (
+            <>
+              <Image source={{ uri: image }} style={styles.camera} />
+              <TouchableOpacity
+                onPress={resetPicture}
+                style={{
+                  height: 40,
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Entypo name="circle-with-cross" size={28} color={"#333"} />
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: 16,
+                    color: "#333",
+                    marginLeft: 10,
+                  }}
+                >
+                  Cancel
+                </Text>
+              </TouchableOpacity>
+            </>
+          )}
+          <Text>Best match: {bestMatch}</Text>
+          {results.map((result, index) => {
+            return (
+              <Fragment key={index}>
+                <Text style={{ marginTop: 20 }}>
+                  {(result.score * 100).toFixed(2)}%
+                </Text>
+                <Text>{result.species.scientificNameWithoutAuthor}</Text>
+              </Fragment>
+            );
+          })}
         </>
       )}
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "flex-end",
-    width: "100%",
-  },
-  buttonContainer: {
     backgroundColor: "#fff",
-    alignSelf: "flex-end",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  preview: {
-    alignSelf: "stretch",
-    flex: 0.5,
+  camera: {
+    // flex: 1,
+    borderRadius: 20,
+    width: "50%",
+    height: "50%",
   },
 });
--- components/utils/utils.js
+++ components/utils/utils.js
@@ -1,22 +1,24 @@
 import axios from "axios";
 
-export const identifyPlant = (base64Image) => {
-  const data = new FormData();
-  //   data.append("images", [base64Image]);
-  const headers = {
-    "Content-Type": "multipart/form-data",
-  };
-  return axios
-    .post(
-      "https://my-api.plantnet.org/v2/identify/all?api-key=2b10jAG9SVbPKssPx5Fu5T1quu",
-      data,
-      headers
-    )
+export const identifyPlant = (photoUri) => {
+  const formData = new FormData();
+  formData.append("images", {
+    uri: photoUri,
+    name: "image.jpg",
+    type: "image/jpeg",
+  });
+  return axios({
+    url: "https://my-api.plantnet.org/v2/identify/all?api-key=2b10jAG9SVbPKssPx5Fu5T1quu",
+    method: "POST",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    data: formData,
+  })
     .then((res) => {
-      return res;
+      return res.data;
     })
     .catch((err) => {
-      return err;
       console.log(err);
     });
 };
