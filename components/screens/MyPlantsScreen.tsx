571700871e92ee96af5da5547829b32aef51f440
Charlie Storer <charliestorer1997@gmail.com>
2023-06-16 12:47:51 +0100

    Pressable added to each plant card

--- components/screens/MyPlantsScreen.tsx
+++ components/screens/MyPlantsScreen.tsx
@@ -7,10 +7,11 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
+  Pressable,
 } from "react-native";
 import getUserDoc from "../utils/getUserDoc";
 
-export function MyPlantsScreen() {
+export function MyPlantsScreen({ navigation }) {
   const [loading, setLoading] = useState(true);
   const [savedPlants, setSavedPlants] = useState([]);
   let scrollViewRef = useRef(null);
@@ -36,7 +37,9 @@ export function MyPlantsScreen() {
   return (
     <View style={styles.container}>
       {loading ? (
-        <Text style={styles.loading}>Loading...</Text>
+        <View style={styles.loadingContainer}>
+          <Text style={styles.loading}>Loading...</Text>
+        </View>
       ) : (
         <>
           <ScrollView
@@ -45,24 +48,32 @@ export function MyPlantsScreen() {
             ref={scrollViewRef}
           >
             {savedPlants.map((plant) => (
-              <View key={plant.id} style={styles.tile}>
-                <View key={plant.common_name} style={styles.card}>
-                  <Text key={plant.nickname} style={styles.nickname}>
-                    {plant.nickname}
-                  </Text>
-                  <Image
-                    key={plant.name}
-                    source={{ uri: plant.Image }}
-                    style={styles.image}
-                  />
-                  <Text key={plant.common_name} style={styles.name}>
-                    {plant.common_name}
-                  </Text>
-                  <Text key={plant.type} style={styles.type}>
-                    {plant.scientific_name}
-                  </Text>
+              <Pressable
+                onPress={() =>
+                  navigation.navigate("Plant Details", {
+                    plantId: plant.id,
+                  })
+                }
+              >
+                <View key={plant.id} style={styles.tile}>
+                  <View key={plant.common_name} style={styles.card}>
+                    <Text key={plant.nickname} style={styles.nickname}>
+                      {plant.nickname}
+                    </Text>
+                    <Image
+                      key={plant.name}
+                      source={{ uri: plant.Image }}
+                      style={styles.image}
+                    />
+                    <Text key={plant.common_name} style={styles.name}>
+                      {plant.common_name}
+                    </Text>
+                    <Text key={plant.type} style={styles.type}>
+                      {plant.scientific_name}
+                    </Text>
+                  </View>
                 </View>
-              </View>
+              </Pressable>
             ))}
             <TouchableOpacity style={styles.button} onPress={handleScrollToTop}>
               <Text>Back to top</Text>
@@ -139,6 +150,10 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  loadingContainer: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
 });
