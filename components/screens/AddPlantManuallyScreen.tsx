6bdb06d4d5109aa6e8fe0c2e88c7106872fab4e1
mik-rzo <michael.panong3@gmail.com>
2023-06-16 14:14:45 +0100

    removed files

--- components/screens/AddPlantManuallyScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export function AddPlantManuallyScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Add Plant Manually</Text>
-    </View>
-  );
-}
