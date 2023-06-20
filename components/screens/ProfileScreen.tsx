6f9a7e64f7c72d0e358a2f98a84836ec4cc3af78
Charlie Storer <charliestorer1997@gmail.com>
2023-06-20 10:54:26 +0100

    Updated log out button

--- components/screens/ProfileScreen.tsx
+++ components/screens/ProfileScreen.tsx
@@ -1,40 +1,59 @@
 import { useContext, useEffect, useState } from "react";
-import { View, Text,Image } from "react-native";
+import { View, Text, Image, Pressable, StyleSheet } from "react-native";
 import getUserProfile from "../utils/getUser";
 import { UserContext } from "../context/User";
 
 type resultsType = {
-  Avatar: string,
+  Avatar: string;
   Location: {
-    longitude?: number,
-    latitude?: number
-  },
-  Username: string
-}
+    longitude?: number;
+    latitude?: number;
+  };
+  Username: string;
+};
 
-export function ProfileScreen() {
-const [userName, setUserName] = useState<string>('');
-const [location, setLocation] = useState<object>({});
-const [avatar, setAvatar] = useState<string>('');
+export function ProfileScreen({ navigation }) {
+  const [userName, setUserName] = useState<string>("");
+  const [location, setLocation] = useState<object>({});
+  const [avatar, setAvatar] = useState<string>("");
+  const [successfullLogOut, setSuccessfulLogout] = useState<boolean>(false);
 
   const { userEmail } = useContext(UserContext);
+
   useEffect(() => {
-    getUserProfile(userEmail).then((result:resultsType) => {
-      setAvatar(result.Avatar);
-      setLocation(result.Location);
-      setUserName(result.Username)
-    });
-  },[])
-  console.log(avatar)
+    if (successfullLogOut) {
+      setSuccessfulLogout(true);
+    } else {
+      getUserProfile(userEmail).then((result: resultsType) => {
+        setAvatar(result.Avatar);
+        setLocation(result.Location);
+        setUserName(result.Username);
+      });
+    }
+  }, [userEmail, successfullLogOut]);
+
+  const handleLogOut = () => {
+    setSuccessfulLogout(true);
+    navigation.navigate("Login Page");
+  };
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Image style={{width:200,height:200}} source={{uri:avatar}}/>
+      <Image style={{ width: 200, height: 200 }} source={{ uri: avatar }} />
       <Text>User Name: {userName}</Text>
       <Text>User Email: {userEmail}</Text>
-      
-   
+      <Pressable style={styles.button} onPress={handleLogOut}>
+        <Text>Logout</Text>
+      </Pressable>
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#2196F3",
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 4,
+    marginBottom: 10,
+  },
+});
