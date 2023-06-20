245c036442466b70f33661ba142aaab5a8efcb90
mik-rzo <michael.panong3@gmail.com>
2023-06-20 10:35:04 +0100

    removed duplicate files

--- utils/capitalise.js
+++
@@ -1,8 +0,0 @@
-export function capitalise(str) {
-  let nameArr = str.split(' ')
-  nameArr = nameArr.map((word) => {
-    return word.charAt(0).toUpperCase() + word.slice(1)
-  })
-  str = nameArr.join(' ')
-  return str
-}
