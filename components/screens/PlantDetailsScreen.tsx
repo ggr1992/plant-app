0ebffd36e4cf51b04c9f666e6b4f6adbb1881c61
mik-rzo <michael.panong3@gmail.com>
2023-06-20 10:50:29 +0100

    font changes to plant details page

--- components/screens/PlantDetailsScreen.tsx
+++ components/screens/PlantDetailsScreen.tsx
@@ -1,5 +1,6 @@
 import { Text, ScrollView, View, SafeAreaView, StyleSheet, Image, ImageSourcePropType, Pressable } from 'react-native'
 import { useState, useEffect, useRef } from 'react'
+import { useFonts } from 'expo-font'
 import { getPlantInfo } from '../utils/getPlantsInfo'
 import { capitalise } from '../utils/capitalise'
 import Icon from 'react-native-vector-icons/AntDesign'
@@ -11,6 +12,13 @@ export function PlantDetailsScreen({ route }) {
 	const [plantDetails, setPlantDetails] = useState({})
 	const [isLoading, setIsLoading] = useState(true)
 	const [displayCareGuide, setDisplayCareGuide] = useState(false)
+
+	const [fontsLoaded] = useFonts({
+		'BDO-Grotesk-Light': require('../../assets/BDOGrotesk-Light.ttf'),
+		'BDO-Grotesk-Reg': require('../../assets/BDOGrotesk-Regular.ttf'),
+		'BDO-Grotesk-Med': require('../../assets/BDOGrotesk-Medium.ttf'),
+		'BDO-Grotesk-Bold': require('../../assets/BDOGrotesk-Bold.ttf')
+	})
 
 	useEffect(() => {
 		getPlantInfo(plantId.toString()).then((plant: object) => {
@@ -70,12 +78,16 @@ export function PlantDetailsScreen({ route }) {
 		})
 	}
 
+	if (!fontsLoaded) {
+		return null
+	}
+
 	return (
 		<View style={styles.page}>
 			<SafeAreaView style={styles.containerWrapper}>
 				<ScrollView contentContainerStyle={styles.container} ref={scrollRef}>
 					<Text style={styles.headerText}>{plantDetails['common_name']}</Text>
-					<Text style={{ ...styles.headerText, fontWeight: 'bold' }}>{plantDetails['scientific_name']}</Text>
+					<Text style={styles.headerTextBold}>{plantDetails['scientific_name']}</Text>
 					{plantDetails['other_name'] && (
 						<Text style={styles.subHeaderText}>Also known as {plantDetails['other_name'].join(', ')}</Text>
 					)}
@@ -84,24 +96,24 @@ export function PlantDetailsScreen({ route }) {
 					</View>
 					{plantDetails['family'] && (
 						<Text style={styles.bodyText}>
-							<Text style={{ fontWeight: 'bold' }}>Family: </Text>
+							<Text style={styles.sectionHeaderText}>Family: </Text>
 							{plantDetails['family']}
 						</Text>
 					)}
 					<Text style={styles.bodyText}>
-						<Text style={{ fontWeight: 'bold' }}>Origin: </Text>
+						<Text style={styles.sectionHeaderText}>Origin: </Text>
 						{plantDetails['origin'].join(', ')}
 					</Text>
 					<Text style={styles.bodyText}>
-						<Text style={{ fontWeight: 'bold' }}>Type: </Text>
+						<Text style={styles.sectionHeaderText}>Type: </Text>
 						{plantDetails['type']}
 					</Text>
 					<Text style={styles.bodyText}>
-						<Text style={{ fontWeight: 'bold' }}>Dimension: </Text>
+						<Text style={styles.sectionHeaderText}>Dimension: </Text>
 						{plantDetails['dimension']}
 					</Text>
 					<Text style={styles.bodyText}>
-						<Text style={{ fontWeight: 'bold' }}>Indoor plant? </Text>
+						<Text style={styles.sectionHeaderText}>Indoor plant? </Text>
 						{plantDetails['indoor'] ? 'Yes' : 'No'}
 					</Text>
 					<Text style={styles.descriptionText}>{plantDetails['plant_description']}</Text>
@@ -116,7 +128,7 @@ export function PlantDetailsScreen({ route }) {
 							<View style={{ width: 30, height: 30 }}>
 								<Icon style={displayCareGuide && styles.iconActive} name='down' size={30} color='black'></Icon>
 							</View>
-							<Text style={{ ...styles.subHeaderText, fontWeight: 'bold' }}>
+							<Text style={styles.subHeaderText}>
 								{displayCareGuide ? 'Hide care guide' : 'View care guide'}
 							</Text>
 						</View>
@@ -136,7 +148,7 @@ export function PlantDetailsScreen({ route }) {
 									alignItems: 'center'
 								}}>
 								<Image style={styles.icon} source={require('../../assets/watering.png')} />
-								<Text style={{ ...styles.bodyText, fontWeight: 'bold' }}>{plantDetails['watering']}</Text>
+								<Text style={styles.sectionHeaderText}>{plantDetails['watering']}</Text>
 							</View>
 							<Text style={styles.descriptionText}>{plantDetails['watering_description']}</Text>
 							<View
@@ -147,7 +159,7 @@ export function PlantDetailsScreen({ route }) {
 									alignItems: 'center'
 								}}>
 								<Image style={styles.icon} source={require('../../assets/sunlight.png')} />
-								<Text style={{ ...styles.bodyText, fontWeight: 'bold' }}>{plantDetails['sunlight'].join(', ')}</Text>
+								<Text style={styles.sectionHeaderText}>{plantDetails['sunlight'].join(', ')}</Text>
 							</View>
 							<Text style={styles.descriptionText}>{plantDetails['sunlight_description']}</Text>
 							{plantDetails['pruning_description'] && (
@@ -160,9 +172,7 @@ export function PlantDetailsScreen({ route }) {
 											alignItems: 'center'
 										}}>
 										<Image style={styles.icon} source={require('../../assets/pruning.png')} />
-										<Text style={{ ...styles.bodyText, fontWeight: 'bold' }}>
-											{plantDetails['maintenance']} maintenance
-										</Text>
+										<Text style={styles.sectionHeaderText}>{plantDetails['maintenance']} maintenance</Text>
 									</View>
 									<Text style={styles.descriptionText}>{plantDetails['pruning_description']}</Text>
 								</>
@@ -177,7 +187,7 @@ export function PlantDetailsScreen({ route }) {
 										alignSelf: 'center'
 									}}>
 									<Image style={styles.icon} source={require('../../assets/soil.png')} />
-									<Text style={{ ...styles.bodyText, fontWeight: 'bold' }}>{plantDetails['soil'].join(', ')}</Text>
+									<Text style={styles.sectionHeaderText}>{plantDetails['soil'].join(', ')}</Text>
 								</View>
 							)}
 							{plantDetails['propagation'] !== 0 && (
@@ -191,9 +201,7 @@ export function PlantDetailsScreen({ route }) {
 										alignSelf: 'center'
 									}}>
 									<Image style={styles.icon} source={require('../../assets/planting.png')} />
-									<Text style={{ ...styles.bodyText, fontWeight: 'bold' }}>
-										{plantDetails['propagation'].join(', ')}
-									</Text>
+									<Text style={styles.sectionHeaderText}>{plantDetails['propagation'].join(', ')}</Text>
 								</View>
 							)}
 						</View>
@@ -206,7 +214,7 @@ export function PlantDetailsScreen({ route }) {
 
 const styles = StyleSheet.create({
 	page: {
-		backgroundColor: '#728c69',
+		backgroundColor: '#ffffff',
 		height: '100%'
 	},
 	containerWrapper: {
@@ -215,8 +223,8 @@ const styles = StyleSheet.create({
 	container: {
 		paddingTop: 5,
 		paddingBottom: 10,
-		marginHorizontal: 10,
-		backgroundColor: '#ffffcd',
+		marginHorizontal: 20,
+		backgroundColor: '#ffffff',
 		rowGap: 8
 	},
 	imageContainer: {
@@ -230,18 +238,31 @@ const styles = StyleSheet.create({
 		borderRadius: 15
 	},
 	headerText: {
+    fontFamily: 'BDO-Grotesk-Med',
 		fontSize: 24,
 		textAlign: 'center'
 	},
+  headerTextBold: {
+    fontFamily: 'BDO-Grotesk-Bold',
+    fontSize: 24,
+		textAlign: 'center'
+  },
 	subHeaderText: {
+    fontFamily: 'BDO-Grotesk-Reg',
 		fontSize: 18,
 		textAlign: 'center'
 	},
+	sectionHeaderText: {
+		fontFamily: 'BDO-Grotesk-Bold',
+		fontSize: 16
+	},
 	bodyText: {
-		fontSize: 18,
+		fontFamily: 'BDO-Grotesk-Reg',
+		fontSize: 16,
 		textAlign: 'left'
 	},
 	descriptionText: {
+		fontFamily: 'BDO-Grotesk-Light',
 		fontSize: 16,
 		textAlign: 'center'
 	},
